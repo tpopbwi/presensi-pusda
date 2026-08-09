438f3cb416ac52a924acaf76ad08e1131c710d25
tpopbwi <tpopbanyuwangi@gmail.com>
2026-08-09 15:38:55 +0700

    Update presensi.js

--- js/presensi.js
+++ js/presensi.js
@@ -75,7 +75,7 @@ async function fetchWithRetry(url, options = {}, retries = 2, delay = 1500) {
 // 1. KONFIGURASI GLOBAL
 // ============================================================
 const GITHUB_LOGO_URL = "https://raw.githubusercontent.com/tpopbwi/presensi-pusda/main/assets/logo.png";
-const API = "https://script.google.com/macros/s/AKfycbxH9YKIi5epf2PvsJL9Whens2URSaZzi1aciTEiyIYitVBvjJP83tNa-B9xaIuN5f-3/exec";
+const API = "https://script.google.com/macros/s/AKfycbxfANwhLfJnT1uDqC_4xIFpCvMDLbM0rZcrFPXqLuFc-u0juCrsTgb7v9yGMUedlWiF/exec";
 
 function getApiUrl(action, params = {}) {
     const url = new URL(API);
